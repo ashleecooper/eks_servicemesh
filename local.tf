1ad74ca98ef80e27a0674386389d41c3fafe16ae
Ashlee Dolan <ashleedolan1@yahoo.com>
2024-08-11 12:51:33 -0500

    Update local.tf

--- local.tf
+++ local.tf
@@ -1,6 +1,6 @@
 locals {
   name   = basename(path.cwd)
-  region = "us-west-2"
+  region = "us-east-1"
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
